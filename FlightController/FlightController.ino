d4f181702ae0a465501dd229a04828390e419538
TheSolverGroup <venkat.chenna31@gmail.com>
2019-10-10 20:37:23 +0530

    Update FlightController.ino

--- FlightController/FlightController.ino
+++ FlightController/FlightController.ino
@@ -35,22 +35,27 @@ float pressure , altitude , altitude_base;
 #define LED_RED 7
 #define LED_GREEN 8
 
-#ifndef servo_x
+/*#ifndef servo_x
   #define servo_x 1
 #endif
 
-#ifndef servo_y
-  #define servo_y 0
-#endif
+#ifndef servo_z
+  #define servo_z 0
+#endif*/
+
+
+int servo_x = 1;
+
+int servo_z = 0;
 
 bool START_STATE = false;
 
-int angle_x , angle_y , prev_x , prev_y;
+int angle_x , angle_z , prev_x , prev_z;
 
 int button_state;
 
 Servo Servo_tvc_x;
-Servo Servo_tvc_y;
+Servo Servo_tvc_z;
 
 
 float GetAngle(float a , int servo_);
@@ -58,12 +63,14 @@ float mod(float x);
 void ChangeServoAngle(int prev_angle , int current_angle , Servo servo);
 bool CheckAltitudeChange(struct altitude alt);
 
-struct Altitude
+struct altitude
 {
-  float prev_altitude;
-  float current_altitude;
+  float prev_alt;
+  float current_alt;
   float base_altitude;
 };
+
+//struct altitude alt;
 
 void setup() {
 
@@ -162,15 +169,15 @@ void setup() {
 
 
    Servo_tvc_x.attach(SERVO_TVC_X);
-   Servo_tvc_y.attach(SERVO_TVC_Y);
+   Servo_tvc_z.attach(SERVO_TVC_Y);
 
    Servo_tvc_x.write(90);
-   Servo_tvc_y.write(90);
+   Servo_tvc_z.write(90);
    
    for(int i = 0;  i<= 180; i++)
    {
     Servo_tvc_x.write(i);
-    Servo_tvc_y.write(i);
+    Servo_tvc_z.write(i);
 
     //delay(25);
     
@@ -179,14 +186,17 @@ void setup() {
    for(int i = 180;  i>=0; i--)
    {
     Servo_tvc_x.write(i);
-    Servo_tvc_y.write(i);
+    Servo_tvc_z.write(i);
 
     //delay(25);
     
    }
 
    Servo_tvc_x.write(90);
-   Servo_tvc_y.write(90); 
+   Servo_tvc_z.write(90); 
+
+   prev_x = 90;
+   prev_z = 90;
 
    Serial.println("all sensors functioning testing done...");
 
@@ -205,68 +215,115 @@ void setup() {
    Serial.println("base altitude is : ");
    Serial.print(altitude_base);
 
-   prev_x = 90;
-   prev_y = 90;
-}
+   
 
-void loop() {
-
-  Wire.beginTransmission(mpu_addr);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(mpu_addr,14,true);
-  ax=Wire.read()<<8|Wire.read();
-  ay=Wire.read()<<8|Wire.read();
-  az=Wire.read()<<8|Wire.read();
-  float Temp=Wire.read()<<8|Wire.read();
-  gx=Wire.read()<<8|Wire.read();
-  gy=Wire.read()<<8|Wire.read();
-  gz=Wire.read()<<8|Wire.read();
+  float Temp;
 
   bmp280.awaitMeasurement();
 
   bmp280.getTemperature(Temp);
   
-  float pressure;
+  //float pressure;
   bmp280.getPressure(pressure);
 
   static float alt;
   bmp280.getAltitude(alt);
 
-  alt = alt - altitude_base;
 
-  // code to debug whether sensors are reporting accurate data
-  /*Serial.println("ax :");
-  Serial.print(ax);
-  Serial.print("ay :");
-  Serial.print(ay);
-  Serial.print("az :");
-  Serial.print(az);
-  Serial.print("altitude_base:");
-  Serial.print(altitude_base);
-  Serial.print("altitude:");
-  Serial.print(alt);
-  Serial.println();
+  bool start_ = Start(alt , altitude_base);
 
-  delay(500);*/
-
-  prev_x = TVS(ax , Servo_tvc_x , servo_x , prev_x);
-
-  if(prev_x == -1)
+  while(!start_)
   {
-    digitalWrite(LED_RED , HIGH);
-    digitalWrite(LED_GREEN , LOW);
-  }
+    
+  //float Temp;
 
-  prev_y = TVS(ay , Servo_tvc_y , servo_y , prev_y);
+  bmp280.awaitMeasurement();
 
-  if(prev_y == -1)
-  {
-    digitalWrite(LED_RED , HIGH);
-    digitalWrite(LED_GREEN , LOW);
-  }
+  bmp280.getTemperature(Temp);
   
-  LogData(ax , ay , az , alt , pressure , Temp);
+  //float pressure;
+  bmp280.getPressure(pressure);
+
+  //static float alt;
+  bmp280.getAltitude(alt);
+
+  start_ = Start(alt , altitude_base);
+
+  }
+
+  Serial.println("Starting Flight Controls....");
+  
+}
+
+void loop() {
+
+  
+  //if(start_)
+  //{
+    
+      float Temp;
+    
+      bmp280.awaitMeasurement();
+    
+      bmp280.getTemperature(Temp);
+      
+      float pressure;
+      bmp280.getPressure(pressure);
+    
+      static float alt;
+      bmp280.getAltitude(alt);
+    
+      float altitude = altitude - altitude_base;
+    
+      Wire.beginTransmission(mpu_addr);
+      Wire.write(0x3B);
+      Wire.endTransmission(false);
+      Wire.requestFrom(mpu_addr,14,true);
+      ax=Wire.read()<<8|Wire.read();
+      ay=Wire.read()<<8|Wire.read();
+      az=Wire.read()<<8|Wire.read();
+      Temp=Wire.read()<<8|Wire.read();
+      gx=Wire.read()<<8|Wire.read();
+      gy=Wire.read()<<8|Wire.read();
+      gz=Wire.read()<<8|Wire.read();
+    
+      
+    
+      // code to debug whether sensors are reporting accurate data
+      /*Serial.println("ax :");
+      Serial.print(ax);
+      Serial.print("ay :");
+      Serial.print(ay);
+      Serial.print("az :");
+      Serial.print(az);
+      Serial.print("altitude_base:");
+      Serial.print(altitude_base);
+      Serial.print("altitude:");
+      Serial.print(alt);
+      Serial.println();
+    
+      delay(500);*/
+    
+      prev_x = TVS(ax , Servo_tvc_x , servo_x , prev_x);
+    
+      if(prev_x == -1)
+      {
+        digitalWrite(LED_RED , HIGH);
+        digitalWrite(LED_GREEN , LOW);
+      }
+    
+      prev_z = TVS(az , Servo_tvc_z , servo_z , prev_z);
+    
+      if(prev_z == -1)
+      {
+        digitalWrite(LED_RED , HIGH);
+        digitalWrite(LED_GREEN , LOW);
+      }
+      
+      LogData(ax , ay , az , alt , pressure , Temp);
+
+  //}
+
 }
 
 int TVS(float a , Servo servo , int servo_ , int prev_angle)
@@ -379,22 +436,46 @@ float GetAngle(float a , int servo_)
 
 
 //RETURN ALTITUDE
-float GetAltitude()
+float GetAltitude(float base_altitude)
 {
   
+  bmp280.awaitMeasurement();
+
+  float Temp;
+  
+  bmp280.getTemperature(Temp);
+  
+  float pressure;
+  bmp280.getPressure(pressure);
+
+  static float altitude;
+  bmp280.getAltitude(altitude);
+
+  altitude = altitude - base_altitude;
+
+  return altitude;
+
 }
 
 //START FLIGHT FUNCTION
 
-bool Start()
+bool Start(float base_altitude,float altitude)
 {
-  
-}
+    if(altitude > base_altitude)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+    
+} 
 
 //CHECK THRUST AND IF +VE DEPLOY PARACHUTE
-void CheckThrust(float az)
+void CheckThrust(float az , struct altitude alt)
 {
-  if(az>0)
+  if(az>0 && alt.prev_alt > alt.current_alt)
   {
     DeployParachute();
   }
