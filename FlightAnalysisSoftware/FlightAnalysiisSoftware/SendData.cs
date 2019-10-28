97659e0e13ec6a6f7695307f023e884438bc40b3
TheSolverGroup <venkat.chenna31@gmail.com>
2019-10-28 16:41:22 +0530

    pcb mount

---
+++ FlightAnalysisSoftware/FlightAnalysiisSoftware/SendData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace FlightAnalysiisSoftware
+{
+    class SendData
+    {
+        private static string FileName{get;set;}
+
+        public static SqlConnection con = new SqlConnection();
+        public static SqlCommand cmd = new SqlCommand();
+        public SendData(string file_name)
+        {
+            FileName = file_name;
+        }
+
+        private FileStream OpenFile()
+        {
+            FileStream stream = new FileStream(FileName, FileMode.Open);
+
+            return stream;
+        }
+
+        public string ReadData()
+        {
+            string data = File.ReadAllText(FileName);
+            /*byte[] buffer = { } ;
+
+            
+
+            FileStream stream = OpenFile();
+            
+            while(stream.CanRead)
+            {
+                stream.Read(buffer, 0, 1);
+            }
+
+            data = Encoding.ASCII.GetString(buffer);
+
+            */    
+            return data;
+            
+        }
+
+    }
+}
